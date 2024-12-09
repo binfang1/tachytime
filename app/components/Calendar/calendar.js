a2d71609ed284fe11683799029a9a79df4858b25
ricsaittan <ricky.tan@edu.sait.ca>
2024-12-09 15:50:40 -0700

    Update calendar.js

--- app/components/Calendar/calendar.js
+++ app/components/Calendar/calendar.js
@@ -99,7 +99,7 @@ export default function App() {
                     <h1 className = "text-center text-4xl mt-4">{getMonthYear(date)}</h1>
                     <div className = "flex gap-4 justify-center mt-4">
                         <button type = "button" className = "w-16 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-blue-400 focus:ring-opacity-75" onClick = {decreaseDate}>←</button>
-                        <button type = "button" className = "w-48 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-blue-400 focus:ring-opacity-75" onClick = {getCurrentDay}>Today's Date:<br/>{today.toDateString()}</button>
+                        <button type = "button" className = "w-48 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 disabled:bg-gray-400 focus:ring-blue-400 focus:ring-opacity-75" onClick = {getCurrentDay}>Todays Date:<br/>{today.toDateString()}</button>
                         <button type = "button" className = "w-16 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400  disabled:bg-gray-400 focus:ring-opacity-75 ml-1" onClick = {increaseDate}>→</button>
                     </div>
                     <ul className = "grid grid-cols-7 w-[1000px] mt-4 mb-12 m-auto justify-items-center">
